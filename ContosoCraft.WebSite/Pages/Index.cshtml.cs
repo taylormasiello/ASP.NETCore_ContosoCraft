4916cc29b18f80f6c89b100ce8ffcd639fc1d695
Taylor Masiello <masiellotay@gmail.com>
2022-03-24 14:43:54 -0700

    Data in a Razor Page

--- ContosoCraft.WebSite/Pages/Index.cshtml.cs
+++ ContosoCraft.WebSite/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContosoCraft.WebSite.Models;
+using ContosoCraft.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +13,18 @@ namespace ContosoCraft.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
