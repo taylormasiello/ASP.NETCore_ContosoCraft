bc7221ce28256af7d2f4aed3caeec7b560b0eb35
Taylor Masiello <masiellotay@gmail.com>
2022-03-31 08:13:22 -0700

    Created a "simple" JSON API

--- ContosoCraft.WebSite/Startup.cs
+++ ContosoCraft.WebSite/Startup.cs
@@ -1,6 +1,8 @@
+using ContosoCraft.WebSite.Models;
 using ContosoCraft.WebSite.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ContosoCraft.WebSite
@@ -52,6 +55,12 @@ namespace ContosoCraft.WebSite
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapGet("/products", (context) =>
+                {
+                    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                    return context.Response.WriteAsync(json);
+                });
             });
         }
     }
